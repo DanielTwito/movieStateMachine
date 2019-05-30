b6d7e6f3bbe7891a3f559e595d2f806410ac5b5a
DanielTwito <Daniel.twito7@gmail.com>
2019-05-30 20:25:41 +0300

    project settings

--- src/Requests/RequestState.java
+++ src/Requests/RequestState.java
@@ -6,6 +6,7 @@ public interface RequestState {
     //for the event fileRequest[disk < file.size)
     void fileRequest(int fileSize);
 
+
     //gor the event time(4)[disk > or < file.size]
     void timeEventAfterFourSecond(int fileSize);
 
