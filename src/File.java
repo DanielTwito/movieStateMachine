6a9c4bca03054a27ea1d5fef27064f0324763467
DanielTwito <Daniel.twito7@gmail.com>
2019-05-31 10:30:26 +0300

    add File class

---
+++ src/File.java
@@ -0,0 +1,17 @@
+public class File {
+    private String fileName;
+    private int fileSize;
+
+    public File(String fileName, int fileSize) {
+        this.fileName = fileName;
+        this.fileSize = fileSize;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public int getFileSize() {
+        return fileSize;
+    }
+}
